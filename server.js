1c534ec68464abe57f7b31672fe66d231259603d
phileas-condemine <phileas.condemine@axa.com>
2017-06-27 11:52:27 +0200

    establish connection to a mongodb locally

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var twitter = require('twitter'),
     app = express(),
     http = require('http'),
     server = http.createServer(app),
+    MongoClient = require('mongodb').MongoClient,
     io = require('socket.io').listen(server);
 
 //Setup twitter stream api
@@ -13,31 +14,48 @@ var twit = new twitter({
   access_token_key: '877868908757299200-VAUhyJrVJdaSbaSmo4bS2HTZG4JLUiV',
   access_token_secret: '3JkhVTrzeiACWMjMAoTnG5Ne3kIRFMhtiG6MpmXjq2yys'
 }),
-stream = null;
+stream = null,
+// cd to twitter-streaming-nodejs/data
+// create mongod --dbpath . --port 27017
+
+url_mongo = 'mongodb://localhost:27017/';
+
 
 //Use the default port (for beanstalk) or default to 8081 locally
 server.listen(process.env.PORT || 8081);
 
-//Setup rotuing for app
+//Setup routing for app
 app.use(express.static(__dirname + '/public'));
 /*app.get("/google1bb01cf67460edbd.html",function(req,res){
         res.sendfile('./google1bb01cf67460edbd.html');
   })*/
 //Create web sockets connection.
+
+
 io.sockets.on('connection', function (socket) {
-
   socket.on("start tweets", function() {
-
     if(stream === null) {
       //Connect to twitter stream passing in filter for entire world.
-      twit.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
+      console.log("init-connection to statuses/filter")
+      twit.stream('statuses/filter', {'locations':'35,-10,55,25','track':'accident, crash, driver, fire, alarm, risk, injury, help'}, function(stream) {
           stream.on('data', function(data) {
               // Does the JSON result have coordinates
               if (data.coordinates){
                 if (data.coordinates !== null){
+                  MongoClient.connect(url_mongo, function(err, db) {
+                    if (err){
+                      console.log(err)
+                    }
+                    console.log("Connected correctly to server");
+                    db.collection('tweets').insert(data,function(err,records){
+                      if(err){console.log("error when writing to mongod:"+err)}
+                      console.log(records)
+                    });
+                    db.close();
+                  });
                   //If so then build up some nice json and send out to web sockets
                   var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
-                  console.log("data coordinates: lat" + data.coordinates.coordinates[0] + " lon "+ data.coordinates.coordinates[1]);
+                  // console.log("data coordinates: lat" + data.coordinates.coordinates[0] + " lon "+ data.coordinates.coordinates[1]);
 
                   socket.broadcast.emit("twitter-stream", outputPoint);
 
@@ -66,7 +84,7 @@ io.sockets.on('connection', function (socket) {
 
                   }
                 }
-              } else {console.log("no data input");}
+              } //else {console.log("no data input");}
               stream.on('limit', function(limitMessage) {
                 return console.log(limitMessage);
               });
